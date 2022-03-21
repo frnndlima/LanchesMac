8e056046d7bddd4cf1c910fe9cb3bbdae1b91126
Fernanda Lima <nanda_mary_lima12@hotmail.com>
2022-03-20 22:40:19 -0300

    Parte 4

--- LanchesMac/Controllers/HomeController.cs
+++ LanchesMac/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
-﻿using LanchesMac.Models;
+﻿using LanchesMac.Repositories.Interfaces;
+using LanchesMac.ViewModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace LanchesMac.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ILancheRepository _lancheRepository;
+        public HomeController(ILancheRepository lancheRepository)
         {
-            _logger = logger;
+            _lancheRepository = lancheRepository;
         }
 
-        public IActionResult Index()
+        public ViewResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                LanchesPreferidos = _lancheRepository.LanchesPreferidos
+            };
+            return View(homeViewModel);
+        }
+
+        public ViewResult AccessDenied()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- LanchesMac/Startup.cs
+++ LanchesMac/Startup.cs
@@ -1,4 +1,12 @@
-﻿namespace LanchesMac;
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac;
+
 
 public class Startup
 {
@@ -6,34 +14,81 @@ public class Startup
     {
         Configuration = configuration;
     }
-    public IConfiguration Configuration { get; }    
-}
-public void ConfigureServices(IServiceCollection services)
-{
-    services.AddControllersWithViews();
-}
 
-public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-{
-    if (env.IsDevelopment())
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
     {
-        app.UseDeveloperExceptionPage();
-    } else
-    {
-        app.UseExceptionHandler("/Home/Error");
-        app.UseHsts();
+        services.AddDbContext<AppDbContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddTransient<ILancheRepository, LancheRepository>();
+        services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        //services.AddIdentity<IdentityUser, IdentityRole>()
+        //     .AddEntityFrameworkStores<AppDbContext>()
+        //     .AddDefaultTokenProviders();
+
+        services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Home/AccessDenied");
+
+        //services.Configure<ConfigurationImagens>(Configuration.GetSection("ConfigurationPastaImagens"));
+
+
+
+        //services.AddTransient<IPedidoRepository, PedidoRepository>();
+
+        //services.AddScoped<RelatorioVendasService>();
+        //services.AddScoped<GraficoVendasService>();
+
+        services.AddScoped(sp => CarrinhoCompra.GetCarrinho(sp));
+
+        services.AddControllersWithViews();
+        //services.AddPaging(options =>
+        //{
+        //    options.ViewName = "Bootstrap4";
+        //    options.PageParameterName = "pageindex";
+        //});
+
+        services.AddMemoryCache();
+        services.AddSession();
     }
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
 
-    app.UseRouting();
-
-    app.UseAuthorization();
-
-    app.UseEndpoints(endpoints =>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        endpoints.MapControllers(
-            name: "default",
-            pattern: "{controller=Home}/{action=Index}/{id?}");
-    });
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseSession();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "AdminArea",
+                pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+            endpoints.MapControllerRoute(name: "categoriaFiltro",
+               pattern: "Lanche/{action}/{categoria?}",
+               defaults: new { Controller = "Lanche", action = "List" });
+
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
 }

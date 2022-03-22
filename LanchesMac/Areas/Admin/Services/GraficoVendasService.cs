8ae3ba61611edff12f2b97baa15150d092359e34
Fernanda Lima <nanda_mary_lima12@hotmail.com>
2022-03-22 09:30:18 -0300

    Ajustes finais

--- LanchesMac/Areas/Admin/Services/GraficoVendasService.cs
+++ LanchesMac/Areas/Admin/Services/GraficoVendasService.cs
@@ -1,9 +1,5 @@
 ﻿using LanchesMac.Context;
 using LanchesMac.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LanchesMac.Areas.Admin.Servicos
 {
